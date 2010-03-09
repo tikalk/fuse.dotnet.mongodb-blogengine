6a6d21a8e8bdbd61c0a764b46c76c09cb61e08d6
manishma <igor.israel@gmail.com>
2010-03-09 14:30:09 +0200

    implement DataStore

--- MongoDBBlogProvider/DataStore.cs
+++ MongoDBBlogProvider/DataStore.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using MongoDB.Driver;
+using System.Xml.Serialization;
+using System.IO;
 
 namespace Tikal
 {
@@ -11,29 +13,59 @@ namespace Tikal
 
         public override object LoadFromDataStore(BlogEngine.Core.DataStore.ExtensionType exType, string exId)
         {
-            var db = ConnectDB();
+            using (var mongo = new MongoDbWr())
+            {
+                var coll = mongo.BlogDB.GetCollection("DataStoreSettings");
 
-            var coll = db.GetCollection("DataStoreSettings");
-            
-            Document spec = new Document();
-            spec["ExtensionType"] = exType;
-            spec["ExtensionId"] = exId;
+                Document spec = new Document();
+                spec["ExtensionType"] = exType;
+                spec["ExtensionId"] = exId;
 
-            var res = coll.FindOne(spec);
-            if(res == null)
-                return null;
+                var res = coll.FindOne(spec);
+                if (res == null)
+                    return null;
 
-            return res["Settings"];
+                return res["Settings"];
+            }
         }
 
         public override void RemoveFromDataStore(BlogEngine.Core.DataStore.ExtensionType exType, string exId)
         {
-            throw new NotImplementedException();
+            using (var mongo = new MongoDbWr())
+            {
+                var coll = mongo.BlogDB.GetCollection("DataStoreSettings");
+                
+                Document spec = new Document();
+                spec["ExtensionType"] = exType;
+                spec["ExtensionId"] = exId;
+                
+                coll.Delete(spec);
+            }
         }
 
         public override void SaveToDataStore(BlogEngine.Core.DataStore.ExtensionType exType, string exId, object settings)
         {
-            throw new NotImplementedException();
+            XmlSerializer xs = new XmlSerializer(settings.GetType());
+            string objectXML = string.Empty;
+            using (StringWriter sw = new StringWriter())
+            {
+                xs.Serialize(sw, settings);
+                objectXML = sw.ToString();
+            }
+
+            using (var mongo = new MongoDbWr())
+            {
+                var coll = mongo.BlogDB.GetCollection("DataStoreSettings");
+
+                Document spec = new Document();
+                spec["ExtensionType"] = exType;
+                spec["ExtensionId"] = exId;
+
+                var res = new Document();
+                res["Settings"] = objectXML;
+
+                coll.Update(res, spec, UpdateFlags.Upsert);
+            }
         }
 
     }
