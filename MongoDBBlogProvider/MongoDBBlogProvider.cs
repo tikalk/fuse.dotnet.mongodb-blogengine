1727b11864be96267d97a9fd9414adaf02ccb803
manishma <igor.israel@gmail.com>
2010-03-09 11:21:04 +0200

    create mongoDbBlogProvider project

---
+++ MongoDBBlogProvider/MongoDBBlogProvider.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BlogEngine.Core.Providers;
+
+namespace Tikal
+{
+    public class MongoDBBlogProvider : BlogProvider
+    {
+        public override void DeleteBlogRollItem(BlogEngine.Core.BlogRollItem blogRollItem)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void DeleteCategory(BlogEngine.Core.Category category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void DeletePage(BlogEngine.Core.Page page)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void DeletePost(BlogEngine.Core.Post post)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void DeleteProfile(BlogEngine.Core.AuthorProfile profile)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override List<BlogEngine.Core.BlogRollItem> FillBlogRoll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override List<BlogEngine.Core.Category> FillCategories()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override List<BlogEngine.Core.Page> FillPages()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override List<BlogEngine.Core.Post> FillPosts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override List<BlogEngine.Core.AuthorProfile> FillProfiles()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override List<BlogEngine.Core.Referrer> FillReferrers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void InsertBlogRollItem(BlogEngine.Core.BlogRollItem blogRollItem)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void InsertCategory(BlogEngine.Core.Category category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void InsertPage(BlogEngine.Core.Page page)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void InsertPost(BlogEngine.Core.Post post)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void InsertProfile(BlogEngine.Core.AuthorProfile profile)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void InsertReferrer(BlogEngine.Core.Referrer referrer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override object LoadFromDataStore(BlogEngine.Core.DataStore.ExtensionType exType, string exId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override System.Collections.Specialized.StringCollection LoadPingServices()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override System.Collections.Specialized.StringDictionary LoadSettings()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override System.Collections.Specialized.StringCollection LoadStopWords()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void RemoveFromDataStore(BlogEngine.Core.DataStore.ExtensionType exType, string exId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void SavePingServices(System.Collections.Specialized.StringCollection services)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void SaveSettings(System.Collections.Specialized.StringDictionary settings)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void SaveToDataStore(BlogEngine.Core.DataStore.ExtensionType exType, string exId, object settings)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override BlogEngine.Core.BlogRollItem SelectBlogRollItem(Guid Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override BlogEngine.Core.Category SelectCategory(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override BlogEngine.Core.Page SelectPage(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override BlogEngine.Core.Post SelectPost(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override BlogEngine.Core.AuthorProfile SelectProfile(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override BlogEngine.Core.Referrer SelectReferrer(Guid Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string StorageLocation()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void UpdateBlogRollItem(BlogEngine.Core.BlogRollItem blogRollItem)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void UpdateCategory(BlogEngine.Core.Category category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void UpdatePage(BlogEngine.Core.Page page)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void UpdatePost(BlogEngine.Core.Post post)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void UpdateProfile(BlogEngine.Core.AuthorProfile profile)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void UpdateReferrer(BlogEngine.Core.Referrer referrer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
